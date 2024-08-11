020e211fcdfdbb5dd6fa5283dc26325307306cf5
Connor Dirks <cdirks4@me.com>
2024-08-11 16:08:59 -0400

    fix: remove action

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  output: "export",
-
-  basePath: "/slander-web",
-  reactStrictMode: true,
-
   typescript: {
     ignoreBuildErrors: true,
   },
